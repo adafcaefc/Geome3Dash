2bf1e1276bc5f4ec3514b678f944a9a59db64d9e
adaf <adafcaefc@gmail.com>
2024-11-29 20:38:48 +0700

    .

--- bin/package_adaf.bat
+++ bin/package_adaf.bat
@@ -1,2 +1,2 @@
 7z\7z.exe a -tzip "C:\Program Files (x86)\Steam\steamapps\common\Geometry Dash\geode\mods\rainixgd.geome3dash.geode" -mx1 -x!7z -x!package_adaf.bat
-
+"C:\Program Files (x86)\Steam\steamapps\common\Geometry Dash\GeometryDash.exe"
