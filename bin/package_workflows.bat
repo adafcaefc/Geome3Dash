e8c0b91ac87990e3b3fb0a8917739064ae53805b
adafcaefc <33832514+adafcaefc@users.noreply.github.com>
2024-12-02 14:03:11 +0700

    Create package_workflows.bat

---
+++ bin/package_workflows.bat
@@ -0,0 +1,1 @@
+7z\7z.exe a -tzip "rainixgd.geome3dash.geode" -mx9 -x!7z -x!package_adaf.bat -x!package_adaf_mx9.bat -x!package_workflows.bat -x!rainixgd.geome3dash.exp -x!rainixgd.geome3dash.lib -x!list.bat
