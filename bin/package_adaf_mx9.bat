5e921b7a6af0c5c0ddf743537fb39997e3b2678f
adaf <adafcaefc@gmail.com>
2024-12-02 15:18:17 +0700

    some build updates

--- bin/package_adaf_mx9.bat
+++
@@ -1,2 +0,0 @@
-7z\7z.exe a -tzip "C:\Program Files (x86)\Steam\steamapps\common\Geometry Dash\geode\mods\rainixgd.geome3dash.geode" -mx9 -x!7z -x!package_adaf.bat -x!package_adaf_mx9.bat -x!package_workflows.bat -x!rainixgd.geome3dash.exp -x!rainixgd.geome3dash.lib -x!list.bat
-"C:\Program Files (x86)\Steam\steamapps\common\Geometry Dash\GeometryDash.exe"
