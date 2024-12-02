f92102d8b5aeecb28f693ed52b3ffdf2ba14a288
adaf <adafcaefc@gmail.com>
2024-12-02 17:47:14 +0700

    auto copy readme

--- bin/run.bat
+++ bin/run.bat
@@ -1,2 +1,4 @@
+if exist "about.md" del "about.md"
+copy "..\README.md" "about.md"
 7z\7z.exe a -tzip "C:\Program Files (x86)\Steam\steamapps\common\Geometry Dash\geode\mods\rainixgd.geome3dash.geode" -mx1 -x!7z -x!rainixgd.geome3dash.exp -x!rainixgd.geome3dash.lib -x!*.bat
 "C:\Program Files (x86)\Steam\steamapps\common\Geometry Dash\GeometryDash.exe"
