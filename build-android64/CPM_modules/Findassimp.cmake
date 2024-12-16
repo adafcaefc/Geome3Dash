8593f4f04edb3d2c62e360c815db15c3a9c2ef1c
adafcaefc <33832514+adafcaefc@users.noreply.github.com>
2024-12-16 20:54:04 +0700

    .

--- build-android64/CPM_modules/Findassimp.cmake
+++
@@ -1,3 +0,0 @@
-include("C:/Users/Windows/Documents/GitHub/Geome3Dash/build-android64/cmake/CPM_0.40.2.cmake")
-CPMAddPackage("GITHUB_REPOSITORY;assimp/assimp;GIT_TAG;v5.4.3;EXCLUDE_FROM_ALL;YES;SYSTEM;YES;")
-set(assimp_FOUND TRUE)
--- build-android64/CPM_modules/Findjson.cmake
+++
@@ -1,3 +0,0 @@
-include("C:/Users/Windows/Documents/GitHub/Geome3Dash/build-android64/cmake/CPM_0.40.2.cmake")
-CPMAddPackage("GITHUB_REPOSITORY;geode-sdk/json;VERSION;3.1.0;EXCLUDE_FROM_ALL;YES;SYSTEM;YES;")
-set(json_FOUND TRUE)
